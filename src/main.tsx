0b335de673bf763f20c3f17538384b5e071b86dd
versicheRunge <thommyhilft@gmx.de>
2025-07-08 23:40:08 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// src/main.tsx
+// KORRIGIERTE VERSION
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// DIESE ZEILE WURDE KORRIGIERT: .tsx wurde entfernt
+import App from './App'; 
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
